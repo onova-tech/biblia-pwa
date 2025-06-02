98047ad4260fe11bb6ede9953a52da71ecaf1f18
onova-tech <onova.tech@gmail.com>
2025-06-01 22:03:17 -0300

    Ajuste

--- js/service_worker_enabler.js
+++ js/service_worker_enabler.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/biblia-pwa/service_worker.js", { scope: '/' })
+    navigator.serviceWorker.register("/biblia-pwa/service_worker.js", { scope: '/biblia-pwa/' })
     .then(registration => {
         console.log('Service worker registered:', registration);
       })
